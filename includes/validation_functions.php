16ae4835c289fae5db7e060aafea5f5089dd5148
Mohammad Javidi <m.javidii@yahoo.com>
2020-07-25 12:14:45 +0430

    update validation function

--- includes/validation_functions.php
+++ includes/validation_functions.php
@@ -1,44 +1,51 @@
-<?php 
+<?php
+
 $errors = array();
 
-function fieldname_as_text($fieldname){
-	$fieldname = str_replace("_", " ", $fieldname);
-	$fieldname = ucfirst($fieldname);
-	return $fieldname;
+function fieldname_as_text($fieldname) {
+  $fieldname = str_replace("_", " ", $fieldname);
+  $fieldname = ucfirst($fieldname);
+  return $fieldname;
 }
-//2.5
-function has_presence($value){
+
+// * presence
+// use trim() so empty spaces don't count
+// use === to avoid false positives
+// empty() would consider "0" to be empty
+function has_presence($value) {
 	return isset($value) && $value !== "";
 }
-//2
-function validation_presence($require_fields){
-	global $errors;
-	foreach ($require_fields as $field) {
-		$value = trim($_POST[$field]);
-		if (!has_presence($value)) {
-			$errors[$field] = fieldname_as_text($field) . " Can't be blank";
-		}
-	}
+
+function validate_presences($required_fields) {
+  global $errors;
+  foreach($required_fields as $field) {
+    $value = trim($_POST[$field]);
+  	if (!has_presence($value)) {
+  		$errors[$field] = fieldname_as_text($field) . " can't be blank";
+  	}
+  }
 }
-//1.5 - and 3
-function has_max_length($value, $max){
+
+// * string length
+// max length
+function has_max_length($value, $max) {
 	return strlen($value) <= $max;
 }
-//1
-function validate_max_lengths($field_with_max_lenght){
+
+function validate_max_lengths($fields_with_max_lengths) {
 	global $errors;
-	// Expects an assoc, array
-	foreach ($field_with_max_lenght as $field => $max) {
+	// Expects an assoc. array
+	foreach($fields_with_max_lengths as $field => $max) {
 		$value = trim($_POST[$field]);
-		if (!has_max_length($value, $max)) {
-			$errors[$field] = fieldname_as_text($field) . " is toolong";
-		}
+	  if (!has_max_length($value, $max)) {
+	    $errors[$field] = fieldname_as_text($field) . " is too long";
+	  }
 	}
 }
 
 // * inclusion in a set
-function has_inclusion_in($value, $set){
+function has_inclusion_in($value, $set) {
 	return in_array($value, $set);
 }
 
- ?>
+?>
