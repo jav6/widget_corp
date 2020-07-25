54fa1dd2553ee724a727cdc888bf118a153dcb83
Mohammad Javidi <m.javidii@yahoo.com>
2020-07-25 12:18:38 +0430

    update admin.php

--- public/admin.php
+++ public/admin.php
@@ -1,13 +1,17 @@
 <?php require_once("../includes/functions.php"); ?>
 <?php include("../includes/layouts/header.php"); ?>
+<?php confirm_logged_in(); ?>
 
-	<div id="main">
-		<div id="navigation">
-		&nbsp;
-	</div>
-	<div id="page">
-		<h2>Admin Menu</h2>
-		<p>Welcome to the admin area.</p>
+<?php $layout_context = "admin"; ?>
+<?php include("../includes/layouts/header.php"); ?>
+
+<div id="main">
+  <div id="navigation">
+    &nbsp;
+  </div>
+  <div id="page">
+    <h2>Admin Menu</h2>
+    <p>Welcome to the admin area, <?php echo htmlentities($_SESSION["username"]); ?>.</p>
 		<ul>
 			<li><a href="manage_content.php">Manage Website Content</a></li>
 			<li><a href="manage_admins.php">Manage Admin Users</a></li>
